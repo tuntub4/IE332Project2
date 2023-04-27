9226d1ebed791c7bb0c0eef3318a3e3a4d1a8a95
HongyiShen <130709450+HongyiShen@users.noreply.github.com>
2023-04-27 16:14:42 -0400

    Update ALGO_works.R

--- ALGO_works.R
+++ ALGO_works.R
@@ -2,14 +2,14 @@
 # Load the necessary packages
 library(imager)
 
-# Define the input and output directories
+# Setting the input and output directories
 input_dir <- "/home/jupyter/dandelions"
 output_dir <- "/home/jupyter/dan2"
 
-# Get a list of all image files in the input directory
+# Scaning image files in the input directory and making them into a list
 image_files <- list.files(input_dir, pattern = "\\.(jpg|jpeg|png)$", full.names = TRUE)
 
-# Create the output directory if it doesn't exist
+# Create the output directory if it doesn't exist, so it's not necessary to create a new folder by hand
 if (!dir.exists(output_dir)) {
   dir.create(output_dir)
 }
@@ -22,28 +22,28 @@ for (i in seq_along(image_files)) {
   # Resize the image
   img <- resize(img, 224, 224)
   
-  # Calculate the threshold value for the 99th percentile of pixel values
+  # Calculate the threshold value for the 99th percentile of pixel values, this will set a threhold for 1% pixels with highest red value.
   threshold <- quantile(img, probs = 0.99, na.rm = TRUE)
   
   # Replace the pixels above the threshold with blue
-  img[img >= threshold] <- c(0, 0, 1) # Set blue channel to 1, and red and green channels to 0
+  img[img >= threshold] <- c(0, 0, 1) # Setting the blue channel to 1, and the red and green channels to 0
   
   output_file <- file.path(output_dir, paste0("image_", i, ".jpg"))
   save.image(img, output_file)
 }
 
-# Print a message indicating that the operation is complete
-cat("Dandelions Pixel replacement operation complete. Modified images saved in", output_dir, "\n")
+# Print a message which shows that the operation is complete
+cat("Dandelions Pixel replacement shift complete. Modified images saved in", output_dir, "\n")
 
 
 
 
 # Do the operation again for grass
-# Define the input and output directories
+# Setting the input and output directories
 input_dir <- "/home/jupyter/grass"
 output_dir <- "/home/jupyter/gra2"
 
-# Get a list of all image files in the input directory
+# Scaning image files in the input directory and making them into a list
 image_files <- list.files(input_dir, pattern = "\\.(jpg|jpeg|png)$", full.names = TRUE)
 
 # Create the output directory if it doesn't exist
@@ -67,7 +67,7 @@ for (i in seq_along(image_files)) {
   threshold <- quantile(green_pixels, probs = 0.99, na.rm = TRUE)
   
   # Replace the most green pixels with yellow
-  yellow_pixels <- c(1, 1, 0) # Set red channel to 1, and green and blue channels to 0
+  yellow_pixels <- c(1, 1, 0) # Setting red channel to 1, and green and blue channels to 0
   img[,,1][green_pixels >= threshold] <- yellow_pixels[1]
   img[,,2][green_pixels >= threshold] <- yellow_pixels[2]
   img[,,3][green_pixels >= threshold] <- yellow_pixels[3]
@@ -76,5 +76,5 @@ for (i in seq_along(image_files)) {
   save.image(img, output_file)
 }
 
-# Print a message indicating that the operation is complete
-cat("Grass Pixel replacement operation complete. Modified images saved in", output_dir, "\n")
+# Print a message which shows that the operation is complete
+cat("Grass Pixel shift operation complete. Modified images saved in", output_dir, "\n")
